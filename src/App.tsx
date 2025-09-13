52c3780955ea57b86e6aede897d00429e061750e
Ryan Bednar <bednar85@gmail.com>
2025-09-12 20:50:04 -0400

    Styling updates

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { moviesList } from './lib/data'
 function App() {
   return (
     <Box backgroundColor="#FFFFFF" width="100vw" height="100vh">
-      <Container className="window-frame" backgroundColor="#657977" p="7" pt="20">
+      <Container className="window-frame" backgroundColor="#657977" p="7" py="20">
         <MovieDetails movie={moviesList[0]} />
         <SimpleGrid columns={[2, 3, 4]} gap="40px">
           {moviesList.map((movie) => (<MovieCard key={movie.id} movie={movie} />))}
--- src/components/molecules/movie-details.tsx
+++ src/components/molecules/movie-details.tsx
@@ -14,9 +14,11 @@ export const MovieDetails: FC<Props> = ({ movie }) => {
   const isSubtitled = (movie.primaryLanguage !== 'English' && !!flagCode);
 
   return (
-    <>
-      {/* add that curvey svg here */}
-      <Card.Root backgroundColor="#FFFFFF" borderRadius="0" borderTopRightRadius="30px" variant="subtle" maxW="lg" position="fixed" bottom="39px" left="40px" zIndex="1" p="7">
+    <Box position="fixed" bottom="40px" left="40px" zIndex="1">
+      <Box w="30px" position="absolute" top="-30px" left="0" transform="rotate(-90deg)">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="m100,0H0v100C0,44.77,44.77,0,100,0Z" fill="#FFFFFF"></path></svg>
+      </Box>
+      <Card.Root backgroundColor="#FFFFFF" borderRadius="0" borderTopRightRadius="30px" variant="subtle" maxW="lg"  p="7">
         <Card.Title mb="3">{movie.title} ({movie.year})</Card.Title>
         <Card.Description mb="3" lineClamp={6}>
           {movie.description}
@@ -29,7 +31,9 @@ export const MovieDetails: FC<Props> = ({ movie }) => {
           </Box>
         )}
       </Card.Root>
-      {/* add that curvey svg here */}
-    </>
+      <Box w="30px" position="absolute" bottom="0" right="-30px" transform="rotate(-90deg)">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="m100,0H0v100C0,44.77,44.77,0,100,0Z" fill="#FFFFFF"></path></svg>
+      </Box>
+    </Box>
   )
 }
